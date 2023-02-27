ed2e142ee0ef8c5627a615409b11cd70e0ae45b1
Henry <15941356654@163.com>
2023-02-27 21:51:27 +0800

    增加课程分类上传封面功能

--- src/views/edu/course/info.vue
+++ src/views/edu/course/info.vue
@@ -65,18 +65,18 @@
 
       <!-- 课程封面 TODO -->
       <!-- 课程封面-->
-<!--      <el-form-item label="课程封面">-->
+      <el-form-item label="课程封面">
 
-<!--        <el-upload-->
-<!--          :show-file-list="false"-->
-<!--          :on-success="handleAvatarSuccess"-->
-<!--          :before-upload="beforeAvatarUpload"-->
-<!--          :action="BASE_API+'/eduoss/fileoss'"-->
-<!--          class="avatar-uploader">-->
-<!--          <img :src="courseInfo.cover">-->
-<!--        </el-upload>-->
+        <el-upload
+          :show-file-list="false"
+          :on-success="handleAvatarSuccess"
+          :before-upload="beforeAvatarUpload"
+          :action="BASE_API+'/eduoss/fileoss'"
+          class="avatar-uploader">
+          <img :src="courseInfo.cover">
+        </el-upload>
 
-<!--      </el-form-item>-->
+      </el-form-item>
 
       <el-form-item label="课程价格">
         <el-input-number :min="0" v-model="courseInfo.price" controls-position="right" placeholder="免费课程请设置为0元"/> 元
@@ -104,9 +104,10 @@ export default {
         teacherId: '',
         lessonNum: 0,
         description: '',
-        cover: '/static/01.jpg',
+        cover: '/static/02.jpg',
         price: 0
       },
+      BASE_API: process.env.BASE_API, // 接口API地址
       teacherList:[],//封装所有的讲师
       subjectOneList:[],//一级分类
       subjectTwoList:[]//二级分类
@@ -121,6 +122,23 @@ export default {
 
   },
   methods:{
+    //上传封面成功调用的方法
+    handleAvatarSuccess(res, file) {
+      this.courseInfo.cover = res.data.url
+    },
+    //上传之前调用的方法
+    beforeAvatarUpload(file) {
+      const isJPG = file.type === 'image/jpeg'
+      const isLt2M = file.size / 1024 / 1024 < 2
+
+      if (!isJPG) {
+        this.$message.error('上传头像图片只能是 JPG 格式!')
+      }
+      if (!isLt2M) {
+        this.$message.error('上传头像图片大小不能超过 2MB!')
+      }
+      return isJPG && isLt2M
+    },
     //点击某个一级分类，触发change，显示对应二级分类
     subjectLevelOneChanged(value) {
       //value就是一级分类id值
